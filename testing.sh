b21cb393cc5fe3b6c289abb370491fd2923f9301
famrashel <aduh_fam@yahoo.com>
2015-03-09 23:18:48 +0700

    update 1-1 dict tag bug

--- testing.sh
+++ testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT="Fam sedang membaca apakah skripsi miliknya yang tebalnya 100 halaman itu berkilah tentang sesuatu sesuatupun"
+INPUT="Fam sedang membaca apakah skripsi miliknya yang tebalnya 100 halaman itu berkilah tentang sesuatu di kantor"
 rm outputs/res-0000000-*
 echo $INPUT > outputs/res-0000000-input.txt
 perl NER.pl -f=0000000
